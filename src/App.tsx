d75bd7aaab4eb6039faa76f796c0d3dbcb90a148
Nagaraju Poralla <songsbeats13@gmail.com>
2026-02-09 01:38:58 +0530

    fix App.tsx routing and sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import { ScrollProgress } from "@/components/ScrollProgress";
 import { ScrollToTop } from "@/components/ScrollToTop";
 import { NewsletterPopup } from "@/components/NewsletterPopup";
 import { CookieConsent } from "@/components/CookieConsent";
 import { WhatsAppChat } from "@/components/WhatsAppChat";
+
 import Index from "./pages/Index";
 import Pricing from "./pages/Pricing";
 import Faq from "./pages/Faq";
@@ -21,17 +23,22 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        {/* Toast systems */}
+        <Toaster />
+        <Sonner />
+
+        {/* Global UI helpers */}
         <ScrollProgress />
         <ScrollToTop />
         <NewsletterPopup />
         <CookieConsent />
         <WhatsAppChat />
+
+        {/* Routes */}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/pricing" element={<Pricing />} />
@@ -42,12 +49,11 @@ const App = () => (
           <Route path="/blog" element={<Blog />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
